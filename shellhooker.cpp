8012a585357249ecc6c62c67b1eb22ed5f63b227
project-flower <project_flower@hotmail.co.jp>
2017-08-11 01:36:36 +0900

    変数・関数名を適切なものにした

--- shellhooker.cpp
+++ shellhooker.cpp
@@ -7,12 +7,12 @@
 namespace ShellHooker
 {
     static HWINEVENTHOOK hHook = nullptr;
-    static WINEVENTPROC __ShellProc = nullptr;
+    static WINEVENTPROC Proc = nullptr;
 
     // これは、エクスポートされた変数です。
     SHELLHOOKER_API HMODULE hModule = nullptr;
 
-    VOID CALLBACK _ShellProc(HWINEVENTHOOK hWinEventHook, DWORD event, HWND hwnd, LONG idObject, LONG idChild, DWORD idEventThread, DWORD dwmsEventTime);
+    VOID CALLBACK WinEventProc(HWINEVENTHOOK hWinEventHook, DWORD event, HWND hwnd, LONG idObject, LONG idChild, DWORD idEventThread, DWORD dwmsEventTime);
 }
 
 // これは、エクスポートされた関数です。
@@ -41,8 +41,8 @@ SHELLHOOKER_API BOOL CALLBACK ShellHooker::SetHook(DWORD eventMin, DWORD eventMa
         return FALSE;
     }
 
-    __ShellProc = hookProc;
-    hHook = SetWinEventHook(eventMin, eventMax, ShellHooker::hModule, _ShellProc, 0, 0, (WINEVENT_OUTOFCONTEXT | WINEVENT_SKIPOWNPROCESS));
+    Proc = hookProc;
+    hHook = SetWinEventHook(eventMin, eventMax, ShellHooker::hModule, WinEventProc, 0, 0, (WINEVENT_OUTOFCONTEXT | WINEVENT_SKIPOWNPROCESS));
 
     if (!hHook)
     {
@@ -52,7 +52,7 @@ SHELLHOOKER_API BOOL CALLBACK ShellHooker::SetHook(DWORD eventMin, DWORD eventMa
     return TRUE;
 }
 
-VOID CALLBACK ShellHooker::_ShellProc(
+VOID CALLBACK ShellHooker::WinEventProc(
     HWINEVENTHOOK hWinEventHook,
     DWORD         event,
     HWND          hwnd,
@@ -61,5 +61,5 @@ VOID CALLBACK ShellHooker::_ShellProc(
     DWORD         idEventThread,
     DWORD         dwmsEventTime)
 {
-    __ShellProc(hWinEventHook, event, hwnd, idObject, idChild, idEventThread, dwmsEventTime);
+    Proc(hWinEventHook, event, hwnd, idObject, idChild, idEventThread, dwmsEventTime);
 }
